5424dc6c132db677f3c7fa699b86725dcfceeb60
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-17 20:55:58 -0300

    Now with sleuth, all the requests will display with a unique id in the console

--- src/main/java/com/github/pedroluiznogueira/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/github/pedroluiznogueira/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.github.pedroluiznogueira.microservices.currencyexchangeservice.contr
 
 import com.github.pedroluiznogueira.microservices.currencyexchangeservice.model.CurrencyExchange;
 import com.github.pedroluiznogueira.microservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,8 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private CurrencyExchangeRepository currencyExchangeRepository;
 
@@ -21,6 +25,8 @@ public class CurrencyExchangeController {
 
     @GetMapping("currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable ("from") String from, @PathVariable ("to") String to) {
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
+
         CurrencyExchange currencyExchange = currencyExchangeRepository
                 .findByFromAndTo(from, to);
         if (currencyExchange == null) {
