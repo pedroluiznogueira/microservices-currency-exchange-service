f1bce4d788054776643d8991165e8352f6641666
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-17 17:04:11 -0300

    The idea here is to show in the console log that the end-point sample-api was called three times here, that's what the circuit breaker with the @Retry annotation does

--- src/main/java/com/github/pedroluiznogueira/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/com/github/pedroluiznogueira/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,13 +1,24 @@
 package com.github.pedroluiznogueira.microservices.currencyexchangeservice.controller;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
 
-    @GetMapping("sample-api")
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/sample-api")
+    @Retry(name = "default")
     public String sampleApi() {
-        return "sample-api";
+        logger.info("Sample Api call received");
+        ResponseEntity<String> forEntity =  new RestTemplate().getForEntity("http://localhost:8080/some-dummy",
+                String.class);
+        return forEntity.getBody();
     }
 }
