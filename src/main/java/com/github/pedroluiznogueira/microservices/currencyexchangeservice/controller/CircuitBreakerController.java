d8a28a269f3fa12621dc1bf3054f18300941813a
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-17 17:35:32 -0300

    Now, if you recieve a lot of requests and they are coming with an error, the circuit breaker stop calling it and just returns directly the fallback response

--- src/main/java/com/github/pedroluiznogueira/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/com/github/pedroluiznogueira/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package com.github.pedroluiznogueira.microservices.currencyexchangeservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +15,7 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
     public String sampleApi() {
         logger.info("Sample Api call received");
         ResponseEntity<String> forEntity =  new RestTemplate()
