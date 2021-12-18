5725f642b9e6d7e6afa89d8ac2d0102ea97c76c8
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-18 20:48:01 -0300

    Auto load data into h2 database implemented

--- src/main/java/com/github/pedroluiznogueira/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ src/main/java/com/github/pedroluiznogueira/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,6 +2,7 @@ package com.github.pedroluiznogueira.microservices.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class CurrencyExchangeServiceApplication {
--- src/main/java/com/github/pedroluiznogueira/microservices/currencyexchangeservice/repository/CurrencyExchangeRepository.java
+++ src/main/java/com/github/pedroluiznogueira/microservices/currencyexchangeservice/repository/CurrencyExchangeRepository.java
@@ -2,7 +2,9 @@ package com.github.pedroluiznogueira.microservices.currencyexchangeservice.repos
 
 import com.github.pedroluiznogueira.microservices.currencyexchangeservice.model.CurrencyExchange;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
     CurrencyExchange findByFromAndTo(String from, String to);
 }
